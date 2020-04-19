4cc4a2a4693fd13e666672bfaf9cf4e4e086540c
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 05:56:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each{|val| val}
+  array.map {|val| if val.split("").size return true}
 end
 
 def find_the_cheese(array)# code an argument here
