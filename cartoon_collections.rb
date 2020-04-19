a0323c0d075a60ffc5493ca7f8beb6cbd9785c49
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 05:28:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |words|
-    if words.join("").size > 4
+    if words.join(" ").size > 4
       return true
     else
       return false
