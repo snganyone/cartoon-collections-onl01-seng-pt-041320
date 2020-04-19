0e2033765d939b4f8e5c357825a736e18633cef8
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 06:06:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,5 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
-  if array.include?(cheese_types) == true
-    return array
-  else
-    return nil
-  end
-
+  
 end
