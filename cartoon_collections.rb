b76a79e5b55a7cafd834f419fa647b32cbe324f3
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 04:37:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each do |val|
-    puts "#{val}"
+  array.collect do |val, idx|
+    puts "#{idx}#{val}"
   end
 end
 
