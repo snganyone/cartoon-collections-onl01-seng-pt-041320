f156ce64afcf46081c4ecc3b41f66e0b7cde92cb
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 03:42:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  new_arr = []
+  array.collect do |i|
+    arr << i
+  end
+  puts arr
 end
 
 def summon_captain_planet# code an argument here
