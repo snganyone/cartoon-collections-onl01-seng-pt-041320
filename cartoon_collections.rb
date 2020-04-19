64b8ee9cb02681eca8737e78bdb635a19f45cb1d
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 05:01:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.each {|val| puts "#{val.capitalize}!,"}
+  array.each {|val| puts "#{val.titlize}!,"}
 end
 
 def long_planeteer_calls# code an argument here
